0d35dff94fa64b97d19df28fb17dffb477094422
hppRC <hpp.ricecake@gmail.com>
2020-04-13 18:26:14 +0900

    :sparkles: add chapter6 solutions

--- chapter6/51.py
+++ chapter6/51.py
@@ -1,22 +1,29 @@
 import nltk
 from nltk import stem
 from nltk.corpus import stopwords
-stopwords = set(stopwords.words('english'))
+import re
 
+stopwords = set(stopwords.words('english'))
+mapper = {"b": "0", "t": "1", "e": "2", "m": "3"}
+# word_stem = stem.PorterStemmer().stem
 
 def create_features(data) -> list:
-    title, category = data.strip().split("\t")
-    title = title.replace("'", "")
-    word_stem = stem.PorterStemmer().stem
-    replaced = " ".join(
-        map(
-            lambda x: "stopword" if x in stopwords else word_stem(x),
-            nltk.word_tokenize(title)
+    features = []
+    labels = []
+    for title, category in map(lambda x: x.strip().split("\t"), data):
+        replaced = " ".join(
+            map(
+                lambda x: "STOPWORD" if x in stopwords else x,
+                nltk.word_tokenize(re.sub(r"['\"|!,.(){}*-]", "", title))
+            )
         )
-    )
-    return [title, replaced, category]
+        features.append(replaced)
+        labels.append(mapper[category])
+
+    return ["\t".join(row) for row in zip(features, labels)]
 
 
-with open("train.txt") as f, open("train.feature.txt", "w") as f_f:
-    features = [create_features(line) for line in f.readlines()]
-    f_f.write("\n".join(["\t".join(feature) for feature in features]))
+for source, output in [["train.txt", "train.feature.txt"], ["valid.txt", "valid.feature.txt"], ["test.txt", "test.feature.txt"]]:
+    with open(source) as f, open(output, "w") as f_f:
+        features = create_features(f.readlines())
+        f_f.write("\n".join(features))
---
+++ chapter6/52.py
@@ -0,0 +1,47 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.pipeline import Pipeline
+import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+train_df = pd.read_table("train.feature.txt", names=("title", "category"))
+valid_df = pd.read_table("valid.feature.txt", names=("title", "category"))
+test_df = pd.read_table("test.feature.txt", names=("title", "category"))
+
+train_X, train_Y = train_df[["title"]], train_df[["category"]]
+test_X, test_Y = test_df[["title"]], test_df[["category"]]
+
+tfidf = TfidfVectorizer(strip_accents=None,
+                        lowercase=False,
+                        preprocessor=None)
+
+param_grid = [{'vect__ngram_range': [(1, 1)],
+                'vect__stop_words': [None],
+                'vect__tokenizer': [nltk.word_tokenize],
+                'clf__penalty': ['l2'],
+                'clf__C': [1.0, 10.0, 100.0]},
+            {'vect__ngram_range': [(1, 1)],
+                'vect__stop_words': [None],
+                'vect__tokenizer': [nltk.word_tokenize],
+                'vect__use_idf': [False],
+                'vect__norm': [None],
+                'clf__penalty': ['l2'],
+                'clf__C': [1.0, 10.0, 100.0]}
+            ]
+
+lr_tfidf = Pipeline([('vect', tfidf),
+                     ('clf', LogisticRegression(random_state=0))])
+gs_lr_tfidf = GridSearchCV(lr_tfidf, param_grid,
+                           scoring='accuracy',
+                           cv=5, verbose=1, n_jobs=-1)
+
+gs_lr_tfidf.fit(np.ravel(train_X), np.ravel(train_Y))
+
+clf = gs_lr_tfidf.best_estimator_
+print(gs_lr_tfidf.best_params_)
+print(gs_lr_tfidf.best_score_)
+print(clf.score(np.ravel(test_X), np.ravel(test_Y)))
